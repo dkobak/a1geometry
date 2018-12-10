e37b0c5c8d15826e679b09b6c1c36cb27d30f7ae
Dmitry Kobak <dmitry.kobak@neuro.fchampalimaud.org>
2018-12-10 23:13:36 +0100

    Zoo update

--- figureNeuronZoo.m
+++ figureNeuronZoo.m
@@ -9,7 +9,7 @@ figure('Position', [100 100 800 800])
 col = parula12();
 
 exemplaryDatasets = [1 7];
-offsets = [0 7];
+% offsets = [0 7];
 
 for example = 1:2
     d = exemplaryDatasets(example);
@@ -18,7 +18,7 @@ for example = 1:2
     [~,ind] = sort(X,'descend');
     %subplots = bsxfun(@plus, [1:7]' + offsets(example), [0:14:84]);
     for n = 1:50
-        subplot(10,10,n + (example-1)*50) %subplots(n))
+        subplots(n + (example-1)*50) = subplot(10,10, n + (example-1)*50); %subplots(n))
         X = squeeze(nanmean(sum(l.datasets{d}(ind(n),:,:,l.time>0 & l.time<0.15,:),4) / 0.15, 5));
         baseline = squeeze(sum(l.datasets{d}(ind(n),:,:,l.time>-0.15 & l.time<0,:),4) / 0.15);
         baseline = nanmean(baseline(:));
@@ -38,11 +38,21 @@ for example = 1:2
     end
 end
 
+for n=1:100
+    pos = get(subplots(n), 'Position');
+    if n<=50
+        pos(2) = pos(2) + .025;
+    else
+    	pos(2) = pos(2) - .025;
+	end
+    set(subplots(n), 'Position', pos);
+end
+
 axes('Position', [0, 0, 1, 1])
 axis([0 1 0 1])
 hold on
 axis off
-plot([0,1], [.52 .52], 'k', 'LineWidth', 2)
+plot([0.1,0.93], [.52 .52], 'k', 'LineWidth', 2)
 
 h = gcf();
 set(h,'Units','Inches');
